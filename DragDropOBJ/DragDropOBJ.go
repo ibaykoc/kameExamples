556590c6811f2c7988cb7950a1dafa4c0051dfd0
Mochammad Iqbal <mchmmdqbl@pop-os.localdomain>
2019-02-16 13:00:10 +0700

    cleanup

--- DragDropOBJ/DragDropOBJ.go
+++ DragDropOBJ/DragDropOBJ.go
@@ -30,6 +30,7 @@ var models []kame.DrawableModel
 var t float32
 
 func update(timeSinceLastFrame float64) {
+	t += 0.01 * float32(timeSinceLastFrame)
 	i := window.GetInput()
 	if i.GetKeyStat(kame.KeyLeftAlt) == kame.Press && i.GetKeyStat(kame.KeyF4) == kame.Press || i.GetKeyStat(kame.KeyEscape) == kame.Press {
 		window.Close()
@@ -37,7 +38,6 @@ func update(timeSinceLastFrame float64) {
 }
 
 func draw(drawer *kame.Drawer) {
-	t += 0.01
 	for i, model := range models {
 		drawer.DrawAt(model, mgl32.Translate3D(-3+float32(i*3), 0, 0).Mul4(mgl32.HomogRotate3D(t, mgl32.Vec3{0, 1, 0})))
 	}
